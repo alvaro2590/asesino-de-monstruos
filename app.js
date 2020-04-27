d4cfea21a094f707d2aa123660d68934cd6e2857
silvanaesq <silmaesquivel@gmail.com>
2020-04-27 17:23:33 -0300

    Objetivo 3

--- app.js
+++ app.js
@@ -6,22 +6,58 @@ new Vue({
         gameIsRunning: false
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        attack: function(){
+        attack: function () {
+            var damage = this.calculateDamage(3, 10)
+            this.monsterHealth -= damage
+            if (this.checkwin()) {
+                return
+            }
+            this.monsterAttacks()
 
         },
-        specialAttack: function(){
+        specialAttack: function () {
 
         },
-        heal: function(){
+        heal: function () {
 
         },
-        giveUp: function(){
-            gameIsRunning = false
+        giveUp: function () {
+            this.gameIsRunning = false
+        },
+
+        monsterAttacks: function () {
+            var damage = this.calculateDamage(5, 12)
+            this.playerHealth -= damage
+            this.checkwin()
+
+        },
+        calculateDamage: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min)
+
+        },
+        checkwin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('¡Ganaste! ¿Jugar de nuevo?')) {
+                    this.startGame()
+                } else {
+                    this.gameIsRunning = false
+                }
+                return true
+            } else if (this.playerHealth <= 0) {
+                if (confirm('¡Perdiste! ¿Jugar de nuevo?')) {
+                    this.startGame()
+                } else {
+                    this.gameIsRunning = false
+                }
+                return true
+            }
+            return false
         }
+
     }
 })
